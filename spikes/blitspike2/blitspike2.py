74f39167c73411d1ce7d76111938faf11f5e29d0
Stephen Fedele <32551324+strangeprogrammer@users.noreply.github.com>
2020-05-02 02:06:55 -0400

    Created a variant of 'blitspike' to show that blitting an entity to the screen instead of to another entity will render all objects properly (succeeds).

---
+++ spikes/blitspike2/blitspike2.py
@@ -0,0 +1,45 @@
+#!/bin/python3
+
+# Draws a big square on the screen as well as a small square across the big square's right side
+
+import pygame
+
+class entity():
+	def __init__(self, path):
+		self.screen = pygame.image.load(path)
+		self.rect = self.screen.get_rect()
+	
+	def move(self, *args):
+		self.rect = self.rect.move(*args)
+	
+	def blit(self, *args):
+		self.screen.blit(*args)
+	
+	def getall(self):
+		return (self.screen, self.rect)
+
+def main():
+	pygame.init()
+	
+	screen = pygame.display.set_mode()
+	
+	square = entity("./Square.png")
+	bigSquare = entity("./BigSquare.png")
+	
+	square.move(224, 96)
+	bigSquare.move(250, 250)
+	
+	screen.fill((255, 255, 255))
+	screen.blit(square.screen, square.rect.move(*bigSquare.rect.topleft))
+	screen.blit(*bigSquare.getall())
+	pygame.display.flip()
+	
+	noexit = True
+	while noexit:
+		for e in pygame.event.get():
+			if e.type == pygame.QUIT:
+				noexit = False
+	
+	pygame.quit()
+
+main()
