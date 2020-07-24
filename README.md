25749ff0cc42d5a97526fed5b7b04016f4b60c31
Stephen Fedele <32551324+strangeprogrammer@users.noreply.github.com>
2020-07-23 21:24:50 -0400

    Added copyright information to the program and its source files. Added a to-do list.

---
+++ README.md
@@ -0,0 +1,9 @@
+# Fruit Punch
+
+### Description
+
+The Fruit Punch game!  
+
+### Copyright Information
+
+See the file 'LICENSE.txt' for details.
--- spikes/oldspikes/entityspike2/Entity.py
+++ spikes/oldspikes/entityspike2/Entity.py
@@ -1,5 +1,27 @@
 #!/bin/python3
 
+# Copyright (C) 2020 Stephen Fedele <32551324+strangeprogrammer@users.noreply.github.com>
+# 
+# This file is part of Fruit Punch.
+# 
+# Fruit Punch is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# Fruit Punch is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with Fruit Punch.  If not, see <https://www.gnu.org/licenses/>.
+# 
+# Additional terms apply to this file.  Read the file 'LICENSE.txt' for
+# more information.
+
+
+
 from pygame import sprite as pgs
 
 from ConflictMonitor import SymmetricConflictMonitor as BCM
